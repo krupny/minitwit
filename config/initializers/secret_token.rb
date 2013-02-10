ed578032671402485db3eb89ad637f2798c936b6
KPNY <alpha.cyrus@gmail.com>
2013-02-10 04:22:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitwit::Application.config.secret_token = '69e3076bcb9acdefe2b949ee132ec407650bed7fc8d8f4d801faf9f6fe41ea51d4726ef51cbc1ccdfaf00b887f5fd5617bbae4481d679f238cf20241706455a1'
